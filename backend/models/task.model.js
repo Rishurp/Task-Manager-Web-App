5f90f620fede42773676778e884375bc2b46c6ba
Rishurp <rishupandey3102@gmail.com>
2024-10-03 15:22:23 +0530

    initial setup

---
+++ backend/models/task.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const taskSchema = mongoose.Schema({
+    title : {
+        type : String,
+        required : true,
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    status : {
+       type : String ,
+       enum : ['TODO', 'DONE'],
+       default : "TODO",
+        required : true
+    },
+    linkedFile : {
+        type : Buffer,
+        required : false
+    },
+    createdOn : {
+        type : Date,
+        required : true
+    },
+    deadline : {
+        type : Date,
+        required : true 
+    }
+});
+
+const Task = mongoose.model("Task", taskSchema)
+
+module.exports.Task = Task;
